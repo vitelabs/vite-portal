5177b97f1397de53f7ec2b8e9144730e828f6923
niklr <niklr@users.noreply.github.com>
2022-08-14 09:23:30 +0000

    release v0.0.1-alpha.3

--- relayer/version/buildversion.go
+++ relayer/version/buildversion.go
@@ -1,3 +1,3 @@
 package version
-const RELAYER_COMMIT_VERSION = "45d47470326749ce1062dedad0c6b7983ca79ec7"
-const RELAYER_BUILD_VERSION = "v0.0.1-alpha.2"
+const RELAYER_COMMIT_VERSION = "c1e03cbb98d137c34cb506211aba4cb7c37a23f0"
+const RELAYER_BUILD_VERSION = "v0.0.1-alpha.3"

c4a9f5ae034a15b496421dd8da80cf4fc3371a56
niklr <niklr@users.noreply.github.com>
2022-08-17 19:43:17 +0000

    release v0.0.1-alpha.5

--- shared/pkg/version/buildversion.go
+++ shared/pkg/version/buildversion.go
@@ -1,3 +1,3 @@
 package version
-const PROJECT_COMMIT_VERSION = "49126d170b08a334c41a8c6ec2eb72b92f99e121"
-const PROJECT_BUILD_VERSION = "v0.0.1-alpha.4"
+const PROJECT_COMMIT_VERSION = "d9d209ec9ce9fbdcaf13e9960eb0b04a16598a48"
+const PROJECT_BUILD_VERSION = "v0.0.1-alpha.5"
